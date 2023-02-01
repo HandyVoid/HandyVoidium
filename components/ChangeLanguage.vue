e62c7c84a9f7916b0e874c5be1da7dfe6ab92797
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-02-01 17:26:51 +0100

    feat: <ChangeLanguage> component

---
+++ components/ChangeLanguage.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { locale, locales } = useI18n(),
+      switchLocalePath = useSwitchLocalePath()
+</script>
+
+
+
+<template>
+  <div>
+    <!-- Input Search Language -->
+    <NuxtLink v-for="{ code, name } in locales" :key="code" :to="switchLocalePath(code)">
+      {{ name }}
+    </NuxtLink>
+  </div>
+</template>
