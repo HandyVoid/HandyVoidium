5c9a23dfdd683c6ddace6a9fc2070fdc1f8b69a6
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-02-05 19:02:29 +0100

    feat: error page

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  error: Object
+})
+</script>
+
+
+
+<template>
+  <NuxtLayout>
+    <main>
+      <h1>Error: {{ error.statusCode }}</h1>
+      <p>{{ error.message }}</p>
+      <NuxtLink to="/">Return Home</NuxtLink>
+    </main>
+  </NuxtLayout>
+</template>
+
+
+
+<style lang="stylus" scoped>
+main
+  text-align center
+  overflow-wrap anywhere
+  margin 1em
+</style>
