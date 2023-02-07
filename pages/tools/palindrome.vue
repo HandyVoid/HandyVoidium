7ef7a14bef90078ca3d0b6e0ad17329005bcff6f
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-02-07 01:04:12 +0100

    feat: palindrome page template

--- pages/tools/palindrome.vue
+++ pages/tools/palindrome.vue
@@ -12,5 +12,23 @@ useSeoMeta({
 <template>
   <main>
     <h1>{{ t("palindrome.title") }}</h1>
+    <!-- What is a palindrome -->
+    <textarea id="palindrome-text" cols="80" rows="15" />
+    <!-- Output -->
   </main>
 </template>
+
+
+
+<style lang="stylus" scoped>
+main
+  text-align center
+  margin 0 1em 2em 1em
+
+#palindrome-text
+  box-sizing border-box
+  font-family inherit
+  font-size 1.2em
+  max-width 100%
+  padding .5em
+</style>
