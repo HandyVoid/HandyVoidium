f5e29b5631905f5bff746ad20df049262c4c3a60
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-02-07 22:25:57 +0100

    feat(home): a little bit of structuring and styling

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,20 @@ useSeoMeta({
 
 <template>
   <main>
-    <h1>{{ config.public.appName }}</h1>
-    <NuxtLink :to="localePath('/tools/palindrome')">{{ t("palindrome.title") }}</NuxtLink>
+    <hgroup>
+      <h1>{{ config.public.appName }}</h1>
+      <p>{{ t("home.meta.description") }}</p>
+    </hgroup>
+
+    <ul>
+      <li><NuxtLink :to="localePath('/tools/palindrome')">{{ t("palindrome.title") }}</NuxtLink></li>
+    </ul>
   </main>
 </template>
+
+
+
+<style lang="stylus" scoped>
+main
+  margin 0 1em
+</style>
