81f2b2bb3a2645b89608f11be3e8ca23872054d8
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-02-11 13:05:37 +0100

    feat(header): language button

--- components/ChangeLanguage.vue
+++ components/ChangeLanguage.vue
@@ -1,26 +1,25 @@
 <script setup>
 const { locale, locales } = useI18n(),
-      switchLocalePath = useSwitchLocalePath(),
-      //
-      showLangList = ref(false)
+      switchLocalePath = useSwitchLocalePath()
 </script>
 
 
 
 <template>
-  <div id="lang-container" @blur="showLangList = false">
-    <button id="lang-button" @click="showLangList = !showLangList">
+  <div id="lang-container">
+    <button id="lang-button">
       <Icon name="mdi:language" />
     </button>
-    <Transition name="lang-list">
-      <ul id="lang-list" v-show="showLangList">
-        <li v-for="{ code, name } in locales" :key="code">
-          <NuxtLink :to="switchLocalePath(code)" >
-            {{ name }}
-          </NuxtLink>
-        </li>
-      </ul>
-    </Transition>
+    <ul id="lang-list">
+      <li v-for="{ code, name } in locales" :key="code">
+        <NuxtLink :to="switchLocalePath(code)" >
+          {{ name }}
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink>Contribute</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -31,19 +30,25 @@ const { locale, locales } = useI18n(),
 #lang-container
   position relative
   margin-left auto
+  &:not(:hover) > #lang-list
+    visibility hidden
+    opacity 0
+    scale .95
 
-.lang-list-enter-active, .lang-list-leave-active
-  transition opacity .2s
-.lang-list-enter-from, .lang-list-leave-to
-  opacity 0
+#lang-button
+  cursor pointer
 
 #lang-list
   list-style none
   position absolute
+  right 0
   background rgba(0, 10, 20, .75)
   backdrop-filter blur(4px)
+  transform-origin top right
   padding 0
-  margin-top .5em
+  margin 0
+  transition-property visibility, opacity, scale
+  transition-duration .2s
   > li > a
     display inline-block
     padding .2em .4em
