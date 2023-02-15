d4393e4f8fec84f86e635bf7d426f2e9e8152dc6
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-02-15 23:24:01 +0100

    fix: link favicon formats

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,8 +13,8 @@ useHead({
   link: [
     { rel: "mask-icon", href: "/mask-icon.svg", color: "#5bbad5" },
     { rel: "icon", href: "/favicon.ico" },
-    { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.svg" },
-    { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.svg" },
+    { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+    { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
     { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }
   ]
 })
